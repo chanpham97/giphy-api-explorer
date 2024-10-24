83082954c64095639f1eb61aa470afb0f23ff35c
Chan Pham <iamchanpham@gmail.com>
2024-10-24 16:42:49 +0000

    Lab template

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# giphy-api-explorer
+# Authentication Lab
+## Rubric
+- Task 1-3 (1 pt)
+- Task 4 (3 pt)
+    - Definition syntax (1 pt)
+    - Accessing URL (1 pt)
+    - Calling function (1 pt)
+- Task 5 (2 pt)
+    - Correct endpoint
+    - Accessing sticker URL (1 pt)
---
+++ index.js
@@ -0,0 +1,37 @@
+// In this assignment, you will navigate a new API and make two API calls
+// Task 1: Account Set up
+// Find the GIPHY API, make an account, and create your API key
+// MAKE SURE TO SELECT API AND NOT SDK & put "Web" as your platform
+// Save your API key in a variable below
+
+
+// Task 2: Endpoint Identification
+// Under GIPHY API, find the GIF & Sticker endpoints
+// Find the endpoint that lets you search for GIFs 
+// Save the base and path in a variable below
+
+
+// Task 3: Building full request URL
+// Find the request (query) parameters to build the full request URL to search for 1 raccoon GIFs
+// Save it in a variable below
+
+
+// Task 4: Making the API call
+// Define the function to fetch the data and console log the url key
+
+
+
+// Call the function to test it worked
+
+
+// Task 5: Running it back
+// Find the endpoint that returns a random sticker
+// Build the request URL for a coding sticker
+
+
+// Define and call the function that prints just the sticker URL
+
+
+
+
+
